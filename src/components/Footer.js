e325af6cd109b8c280efac5a1117c33c4e3f447c
DoftMoon <DoftMoon@users.noreply.github.com>
2023-12-11 13:48:24 +0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,14 @@ class Footer extends React.Component {
 				<div className={"jsx-1025e0387f6b8ad7 jsx-a32c8b4abb10806f jsx-3304264316 grid-container slim"}>
 					<div className="jsx-2646878052 Footer_fr__OgTKW grid-item">
 						<Link title="ReManga" to="/">
-							<h4 className="Typography_h4__PdogT mb-2">ReManga</h4>
+							<h4 className="Typography_h4__PdogT mb-2">Manga</h4>
 						</Link>
 						<h1 className="Typography_body1__YTqxB" style={{display:"none"}}>Читать мангу онлайн</h1>
 						<p className="Typography_body2__piveF Typography_color-textSecondary__iFFB7 mb-2">Всегда готовы ответить на вопросы</p>
-						<a href="https://vk.com/im?sel=-185404064" className="hoverLink" title="Задать вопрос" target="_blank" rel="noreferrer">Задать вопрос</a>
+						<a href="https://vk.com/" className="hoverLink" title="Задать вопрос" target="_blank" rel="noreferrer">Задать вопрос</a>
 						<p className="Typography_body1__YTqxB Typography_color-textSecondary__iFFB7 mt-4 caption">Почта для связи:
-							<br/><a className="Typography_body1__YTqxB Typography_color-textPrimary__Qy1D6 p-0 m-0 link" href="mailto:contact@remanga.org">contact@remanga.org</a></p>
-						<p className="Typography_caption___iNir Typography_color-textSecondary__iFFB7 mt-4 hidden sm:block">remanga.org 2023</p>
+							<br/><a className="Typography_body1__YTqxB Typography_color-textPrimary__Qy1D6 p-0 m-0 link" href="mailto:contact@manga.org">contact@remanga.org</a></p>
+						<p className="Typography_caption___iNir Typography_color-textSecondary__iFFB7 mt-4 hidden sm:block">Babarukin Artem</p>
 					</div>
 					<div className="jsx-2646878052 Footer_fp__9ibj_ grid-item">
 						<h6 className="Typography_h6__VMBDX mb-4 uppercase">Разделы</h6>
